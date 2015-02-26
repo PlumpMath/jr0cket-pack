31eddbbfbe9ead754fcb42113280c21fbd2b0aef
John Stevenson <john@jr0cket.co.uk>
2015-02-26 23:33:55 +0000

    Set Ubuntu Mono font 16 point as default font

---
+++ init.el
@@ -0,0 +1,12 @@
+;; User pack init file
+;;
+;; User this file to initiate the pack configuration.
+;; See README for more information.
+
+
+;; Load in personal code snippets
+;; need to create some snippets first though..
+;; (yas/load-directory "~/.live-packs/jr0cket-pack/etc/snippets" )
+
+;; Set the font family and size for Emacs
+(live-set-default-font "Ubuntu Mono 16")
