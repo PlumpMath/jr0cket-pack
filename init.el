b8607a4f5ff0cabb74b8109766ddd2e01c1ba2bf
John Stevenson <john@jr0cket.co.uk>
2015-02-26 23:41:54 +0000

    Stop jumping text in window when scrolling

--- init.el
+++ init.el
@@ -8,5 +8,12 @@
 ;; need to create some snippets first though..
 ;; (yas/load-directory "~/.live-packs/jr0cket-pack/etc/snippets" )
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General Emacs configuration
+
 ;; Set the font family and size for Emacs
 (live-set-default-font "Ubuntu Mono 16")
+
+;; Stop the buffer jumping when scrolling up and down
+(setq scroll-conservatively 999999)
